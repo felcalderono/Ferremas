94d28937b9677f6d52374c40640d9ff47a22b73e
Felipe <felipe@MacBook-Pro-de-Felipe-2.local>
2026-05-07 18:18:46 -0400

    fix cosplay module

--- Ferreteria/wsgi.py
+++ Ferreteria/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Cosplay.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ferreteria.settings')
 
 application = get_wsgi_application()
