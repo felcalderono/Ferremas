5cbfff97171ae8a528e99bc5b87911d4f6759eea
Felipe <felipe@MacBook-Pro-de-Felipe-2.local>
2026-05-07 21:03:57 -0400

    fix static files

--- Ferreteria/settings.py
+++ Ferreteria/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,6 +139,8 @@ import os
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR,'static'),]
